d19cf66c7ad5fc7d834a488ee4a6820d4e0f5d29
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-20 15:50:34 -0500

    configure cors for mart-pos.vercel.app

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ import routes, { rout1 } from "./routes/routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: {
-      global,
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: "https://mart-pos.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", routes);

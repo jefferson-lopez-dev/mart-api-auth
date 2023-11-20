bffe791f95735be311ef7c85ad16eb521ab8aaf9
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-20 17:13:58 -0500

    configure cors in local

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
 import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
-import routes from "./routes/routes.js";
+import routes, { rout1 } from "./routes/routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: {
-      global,
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 204,
-    allowedHeaders: "Content-Type, Authorization",
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Reemplaza con tu URL de producción
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", routes);
+app.use(rout1);
 
 export default app;

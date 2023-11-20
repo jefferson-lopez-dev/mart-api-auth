1b415e8cde130d2871fbc0b5b96337cbb71f3892
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-20 18:30:54 -0500

    use cors with append

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ import routes, { rout1 } from "./routes/routes.js";
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Reemplaza con tu URL de producción
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
+  res.append("Access-Control-Allow-Origin", ["*"]); // Reemplaza con tu URL de producción
+  res.append("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.append("Access-Control-Allow-Credentials", true);
   next();
 });
 

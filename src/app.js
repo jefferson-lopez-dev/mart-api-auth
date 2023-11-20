34552935df321bebcd672d478dd0e3c903a5a861
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-20 17:03:24 -0500

    configure all cors

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import routes, { rout1 } from "./routes/routes.js";
+import routes from "./routes/routes.js";
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://mart-pos.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  allowedHeaders: "Content-Type, Authorization",
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: {
+      global,
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", routes);
-app.use(rout1);
 
 export default app;

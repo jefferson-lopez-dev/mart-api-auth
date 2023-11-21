808b02d1b386d938dc91df2298418b9994ad4097
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-21 10:19:57 -0500

    connfigure cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import routes, { rout1 } from "./routes/routes.js";
+import cors from "cors";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]); // Reemplaza con tu URL de producción
-  res.append("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  // res.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  // res.append("Access-Control-Allow-Credentials", true);
-  next();
-});
-
+app.use(
+  cors({
+    origin: {
+      global,
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", routes);

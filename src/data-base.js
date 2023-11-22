a8abd31fde2d8c12493797e0a53373aa8737747e
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-22 07:04:02 -0500

    proof

--- src/data-base.js
+++ src/data-base.js
@@ -9,6 +9,7 @@ export const ConnectionDB = async () => {
     const db = await mongoose.connect(
       "mongodb+srv://jefferson__dev:T87alLXy3W8E6z6J@mart-auth.p6ynru1.mongodb.net/mart-auth?retryWrites=true&w=majority"
     );
+    console.log("first");
     console.log(`● DB is connected ${db.connection.name}`.green.bold);
   } catch (error) {
     console.log(`● Disconnected to db ${db.connection.name}`.red.bold, error);

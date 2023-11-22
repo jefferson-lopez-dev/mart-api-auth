56164527d27d84e03c16cd544932b82081ce7bbc
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-22 10:30:14 -0500

    change url

--- src/data-base.js
+++ src/data-base.js
@@ -6,10 +6,7 @@ const URL_DATABASE = process.env.URL_MONGO_DB_ATLAS;
 
 export const ConnectionDB = async () => {
   try {
-    const db = await mongoose.connect(
-      "mongodb+srv://jefferson__dev:T87alLXy3W8E6z6J@mart-auth.p6ynru1.mongodb.net/mart-auth?retryWrites=true&w=majority"
-    );
-    console.log("first");
+    const db = await mongoose.connect(URL_DATABASE);
     console.log(`● DB is connected ${db.connection.name}`.green.bold);
   } catch (error) {
     console.log(`● Disconnected to db ${db.connection.name}`.red.bold, error);

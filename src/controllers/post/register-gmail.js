b1ff34b1a69a2bdca83ebd35616e6688d6c3090c
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-22 16:57:09 -0500

    use .env

--- src/controllers/post/register-gmail.js
+++ src/controllers/post/register-gmail.js
@@ -6,8 +6,6 @@ export const useRegisterGmail = async (req, res) => {
   try {
     const { gmail, password } = req.body;
 
-    return res.json({ message: "Register" });
-
     const gmail_found = await Gmail.findOne({ gmail });
     if (gmail_found) {
       return res.json({
--- src/data-base.js
+++ src/data-base.js
@@ -6,9 +6,7 @@ const URL_DATABASE = process.env.URL_MONGO_DB_ATLAS;
 
 export const ConnectionDB = async () => {
   try {
-    await connect(
-      "mongodb+srv://jefferson__dev:T87alLXy3W8E6z6J@mart-auth.p6ynru1.mongodb.net/mart-auth?retryWrites=true&w=majority"
-    );
+    await connect(URL_DATABASE);
     console.log("● DB is connected");
   } catch (error) {
     console.log("● Disconnected to db", error);
